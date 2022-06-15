9e2ea356cdcd70476a09aaf94dbb62f6b37a3833
Hp <Hp@LAPTOP-RQUEIEH8>
2022-06-15 11:21:56 +0530

    by rsk

--- GIT Project/src/com/cjc/git/Git1.java
+++ GIT Project/src/com/cjc/git/Git1.java
@@ -4,11 +4,11 @@ public class Git1 {
 	
 	public void m1()
 	{
-		system.out.println(" this is m1 method");
+		System.out.println(" this is m1 method");
 	}
 	public void m2()
 	{
-		system.out.println(" this is m2 method");
+		System.out.println(" this is m2 method");
 	}
 	
 	
