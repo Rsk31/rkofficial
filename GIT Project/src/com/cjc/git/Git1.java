75eef65f862c724668038cc28034ee2344cf6e9d
Rsk31 <107532149+Rsk31@users.noreply.github.com>
2022-06-15 11:18:57 +0530

    Update Git1.java

--- GIT Project/src/com/cjc/git/Git1.java
+++ GIT Project/src/com/cjc/git/Git1.java
@@ -1,9 +1,24 @@
 package com.cjc.git;
 
 public class Git1 {
+	
+	public void m1()
+	{
+		system.out.println(" this is m1 method");
+	}
+	public void m2()
+	{
+		system.out.println(" this is m2 method");
+	}
+	
+	
+	
 	public static void main(String[] args) {
 		
 		System.out.println("git project");
+		
+		
+		
 	}
 
 }
